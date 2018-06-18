494ba7741d3cbc80db0ee80efbf0c2c2b717a97f
elliottabirch <elliottabirch@gmail.com>
2018-06-18 16:09:13 -0700

    implement report creation dashboard

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
 class Header extends Component {
   static state = {
@@ -15,16 +15,6 @@ class Header extends Component {
             <a href="#home">Adventure Links</a>
           </Navbar.Brand>
         </Navbar.Header>
-        <Nav>
-          <NavDropdown eventKey={3} title="Reports" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Camp Photos</MenuItem>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <NavDropdown eventKey={3} title="Camp Docs" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Sign-In</MenuItem>
-          </NavDropdown>
-        </Nav>
       </Navbar>
     );
   }
--- src/containers/ReportDashboard/index.jsx
+++ src/containers/ReportDashboard/index.jsx
@@ -1,49 +1,69 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import { Form, FormGroup, ControlLabel, FormControl, Button, Alert, Grid } from 'react-bootstrap';
+import moment from 'moment';
 // import { streamSeasons, streamSessions } from '../../util/reports/data';
 
 class ReportDashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: new Date(),
-      endDate: new Date(),
-      reports: [],
-      sessions: [],
+      startDate: moment(),
+      endDate: moment(),
       loading: false,
+      currentReport: 'getCamps',
     };
   }
 
-  componentDidMount = () => {
-    this.getAllSeasons();
+  runReport = (startDate, endDate) => {
+    this.setState({ err: null });
+    console.log(startDate, endDate, this.state.currentReport);
+    this.toggleLoading();
+    fetch(`https://gsz8psj9gj.execute-api.us-east-1.amazonaws.com/misc/${this.state.currentReport}?startDate=${moment(startDate).format('YYYY-MM-DD')}&endDate=${moment(endDate).format('YYYY-MM-DD')}`, { mode: 'no-cors' })
+      .then(() => this.toggleLoading())
+      .catch((err) => {
+        this.setState({ err });
+        this.toggleLoading();
+      });
   }
-getAllSeasons = () => {
-  console.log();
-  this.toggleLoading();
-  fetch('https://gsz8psj9gj.execute-api.us-east-1.amazonaws.com/misc/campPhotos?startDate=2018-06-10&endDate=2018-06-20')
-    .then(() => this.toggleLoading());
-}
+
 toggleLoading = () => {
   console.log(`toggled loading${this.state.loading}`);
-  this.setState({ loading: !!this.state.loading });
+  this.setState({ loading: !this.state.loading });
 }
+
+changeKey = keyName => (e) => {
+  console.log(keyName, e.target.value);
+  this.setState({ [`${keyName}`]: e.target.value });
+}
+
+
 render() {
   return (
-    <div>
-      {`${this.state.sessions}`}
-      {`${this.state.loading}`}
+    <Grid >
+      {this.state.err && <Alert bsStyle="danger">
+        <strong>Holy guacamole!</strong> {this.state.err.message}
+      </Alert>}
       <Form inline>
         <FormGroup >
+          <ControlLabel>Report Name</ControlLabel>{' '}
+          <FormControl componentClass="select" value={this.state.currentReport} onChange={this.changeKey('currentReport')}>
+            <option value="getCamps">Sign In Sheets</option>
+            <option value="campPhotos">Camp Photos</option>
+          </FormControl>{' '}
           <ControlLabel>Start Date</ControlLabel>{' '}
-          <FormControl type="Date" />
-        </FormGroup>{' '}
-        <FormGroup>
+          <FormControl type="Date" value={this.state.startDate} onChange={this.changeKey('startDate')} />{' '}
           <ControlLabel>End Date</ControlLabel>{' '}
-          <FormControl type="Date" placeholder="jane.doe@example.com" />
+          <FormControl type="Date" value={this.state.endDate} onChange={this.changeKey('endDate')} />
         </FormGroup>{' '}
-        <Button>Filter Camps</Button>
+        <Button
+          disabled={this.state.loading}
+          bsStyle={this.state.loading ? 'warning' : 'success'}
+          onClick={() => this.runReport(this.state.startDate, this.state.endDate)}
+        >
+        Run Reports
+        </Button>
       </Form>
-    </div>
+    </Grid>
   );
 }
 }
