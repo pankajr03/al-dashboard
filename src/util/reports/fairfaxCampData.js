c19ba51ce4c20a327f93dc554e1d8e9d5ebea7f8
Elliott Birch <elliottabirch@gmail.com>
2019-04-27 19:36:37 -0700

    Update fairfaxCampData.js

--- src/util/reports/fairfaxCampData.js
+++ src/util/reports/fairfaxCampData.js
@@ -17,7 +17,7 @@ module.exports = () => hl(fetch(fairfaxData))
   .map(($) => {
     const nodeRows = $('.sessions.panel').find('tbody').find('tr').toArray();
     const textRows = nodeRows.map(row => $(row).find('td').contents().map((i, el) => el.data).toArray().map(a => a.trim()).filter(a => !!a));
-    return textRows.map(([campCode, location, dateRange,, openSpots]) => ({ campCode, location, dateRange, openSpots }));
+    return textRows.map(([campCode, location, dateRange,,, openSpots]) => ({ campCode, location, dateRange, openSpots }));
   })
   .map(rows => [fairFaxHeaders, ...rows])
   .collect()
