2ef9c631e129770ba9f3ed7708fcf5cd90f9d8bc
pankajr03 <pankaj1211ranout@gmail.com>
2020-06-28 19:46:28 +0530

    session name added

--- src/util/reports/fairfaxCampData.js
+++ src/util/reports/fairfaxCampData.js
@@ -1,25 +1,56 @@
-const cheerio = require('cheerio');
-const hl = require('highland');
-const { createBook } = require('./util');
-const { endPoints: { fairfaxData } } = require('./constants');
+const cheerio = require("cheerio");
+const hl = require("highland");
+const { createBook } = require("./util");
+const {
+  endPoints: { fairfaxData }
+} = require("./constants");
 
 const fairFaxHeaders = {
-  campCode: 'Camp Code',
-  location: 'Camp Location',
-  dateRange: 'Date Range',
-  openSpots: 'Open Spots',
+  campCode: "Camp Code",
+  location: "Camp Location",
+  dateRange: "Date Range",
+  openSpots: "Open Spots",
+  sessionName: "Session Name"
 };
 
 console.log(fairfaxData);
-module.exports = () => hl(fetch(fairfaxData))
-  .flatMap(response => hl(response.text()))
-  .map(data => cheerio.load(data))
-  .map(($) => {
-    const nodeRows = $('.sessions.panel').find('tbody').find('tr').toArray();
-    const textRows = nodeRows.map(row => $(row).find('td').contents().map((i, el) => el.data).toArray().map(a => a.trim()).filter(a => !!a));
-    return textRows.map(([campCode, location, dateRange,,, openSpots]) => ({ campCode, location, dateRange, openSpots }));
-  })
-  .map(rows => [fairFaxHeaders, ...rows])
-  .collect()
-  .map(sheets => createBook(sheets, 0, fairFaxHeaders, 'Fairfax Enrollment'))
-  .merge();
+module.exports = () =>
+  hl(fetch(fairfaxData))
+    .flatMap(response => hl(response.text()))
+    .map(data => cheerio.load(data))
+    .map($ => {
+      const nodeRowsH2 = $(".FindTour")
+        .contents()
+        .map((i, el) => el.data)
+        .toArray()
+      
+      nodeRowsH2.forEach(function(i, o) {
+        $("#mainContent_repeaterActivities_activitySnapshot_"+o+"_sessionPanel").find('tr').append('<td>'+i+'</td>');
+
+      });  
+     
+      const nodeRows = $(".sessions.panel")
+        .find("tbody")
+        .find("tr")
+        .toArray();
+      const textRows = nodeRows.map(row =>
+        $(row)
+          .find("td")
+          .contents()
+          .map((i, el) => el.data)
+          .toArray()
+          .map(a => a.trim())
+          .filter(a => !!a)
+      );
+      return textRows.map(([campCode, location, dateRange, , , openSpots,,sessionName]) => ({
+        campCode,
+        location,
+        dateRange,
+        openSpots,
+        sessionName
+      }));
+    })
+    .map(rows => [fairFaxHeaders, ...rows])
+    .collect()
+    .map(sheets => createBook(sheets, 0, fairFaxHeaders, "Fairfax Enrollment"))
+    .merge();
