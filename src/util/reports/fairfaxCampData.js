34d93e468ba037c4e9d99ea0bca10be7887de089
pankajr03 <pankaj1211ranout@gmail.com>
2020-06-28 19:13:52 +0530

    add sessionname in report
    Testing

--- src/util/reports/fairfaxCampData.js
+++ src/util/reports/fairfaxCampData.js
@@ -9,7 +9,8 @@ const fairFaxHeaders = {
   campCode: "Camp Code",
   location: "Camp Location",
   dateRange: "Date Range",
-  openSpots: "Open Spots"
+  openSpots: "Open Spots",
+  sessionName: "Session Name"
 };
 
 console.log(fairfaxData);
@@ -18,6 +19,16 @@ module.exports = () =>
     .flatMap(response => hl(response.text()))
     .map(data => cheerio.load(data))
     .map($ => {
+      const nodeRowsH2 = $(".FindTour")
+        .contents()
+        .map((i, el) => el.data)
+        .toArray()
+      
+      nodeRowsH2.forEach(function(i, o) {
+        $("#mainContent_repeaterActivities_activitySnapshot_"+o+"_sessionPanel").find('tr').append('<td>'+i+'</td>');
+
+      });  
+     
       const nodeRows = $(".sessions.panel")
         .find("tbody")
         .find("tr")
@@ -31,11 +42,12 @@ module.exports = () =>
           .map(a => a.trim())
           .filter(a => !!a)
       );
-      return textRows.map(([campCode, location, dateRange, , , openSpots]) => ({
+      return textRows.map(([campCode, location, dateRange, , , openSpots,,sessionName]) => ({
         campCode,
         location,
         dateRange,
-        openSpots
+        openSpots,
+        sessionName
       }));
     })
     .map(rows => [fairFaxHeaders, ...rows])
