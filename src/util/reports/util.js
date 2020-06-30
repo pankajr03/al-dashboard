fe698c13935c340926d6eaa2d8e19f8149f3213b
pankajr03 <pankaj1211ranout@gmail.com>
2020-06-30 12:46:47 +0530

    New methods added for handling custom Report

--- src/util/reports/util.js
+++ src/util/reports/util.js
@@ -1,44 +1,97 @@
-const hl = require('highland');
-const XLSX = require('xlsx');
-const _ = require('lodash');
+const hl = require("highland");
+const XLSX = require("xlsx");
+const _ = require("lodash");
+const request = require("request");
 
-const { endPoints: { base: api } } = require('./constants');
+const {
+  endPoints: { base: api }
+} = require("./constants");
+
+const post = hl.wrapCallback(request.post);
+
+const {
+  endPoints: { base },
+  baseRequest,
+  baseBody
+} = require("./constants");
 
 const createUrl = (url, endPoint) => `${url}/${endPoint}`;
+const parseBuffer = stream =>
+  stream
+    .collect()
+    .map(buffers => buffers.join(""))
+    .map(res => JSON.parse(res));
 
-const parseBuffer = stream => stream
-  .collect()
-  .map(buffers => buffers.join(''))
-  .map(res => JSON.parse(res));
+const streamData = (query, endPoint) => {
+  const body = Object.assign(
+    { request: Object.assign(query, baseRequest) },
+    baseBody
+  );
+  return hl(
+    post({
+      uri: createUrl(base, endPoint),
+      body,
+      json: true
+    })
+  )
+    .pluck("body")
+    .flatten();
+};
 
-const streamData = (body, endPoint) => hl(fetch(createUrl(api, endPoint), {
-  method: 'POST',
-  body: JSON.stringify(body),
-}))
-  .flatMap(response => hl(response.json()))
-  .pluck('data')
-  .flatten();
+const assignDocumentData = (documents, getter, localKey, foreignKey, as) =>
+  getter({ [`${foreignKey}s`]: _.map(documents, localKey) })
+    .collect()
+    .flatMap(docsToAdd => {
+      const docsToAddByForeignKey = _.keyBy(docsToAdd, foreignKey);
+      return hl(documents).map(document =>
+        Object.assign(
+          {
+            [`${as}`]: docsToAddByForeignKey[document[localKey]] || {}
+          },
+          document
+        )
+      );
+    });
 
-const assignDocumentData = (documents, getter, localKey, foreignKey, as) => getter({ [`${foreignKey}s`]: _.map(documents, localKey) })
-  .collect()
-  .flatMap((docsToAdd) => {
-    const docsToAddByForeignKey = _.keyBy(docsToAdd, foreignKey);
-    return hl(documents)
-      .map(document => Object.assign({
-        [`${as}`]: docsToAddByForeignKey[document[localKey]] || {},
-      }, document));
-  });
-
+  const createBookCustom = (sheets, currentBook, headers, reportName, sessionName) => {
+    let currentSheet = 0;
+    return hl(sheets)
+      .doto(() => {
+        currentSheet += 1;
+      })
+      .reduce((wb, sheet) => {
+      //console.log(sheet[1].campCode); 
+      const nName = sheet[1].seasonName;
+      
+        const ws = XLSX.utils.json_to_sheet(sheet, {
+          header: Object.keys(headers),
+          skipHeader: true
+        });
+        XLSX.utils.book_append_sheet(wb, ws, `${reportName} ${currentSheet} - ` + nName.substr(0,12));
+        return wb;
+      }, XLSX.utils.book_new())
+      .map(wb =>
+        XLSX.writeFile(wb, `${reportName}-book${currentBook}-${new Date()}.xlsx`)
+      );
+  };
+       
 const createBook = (sheets, currentBook, headers, reportName) => {
   let currentSheet = 0;
   return hl(sheets)
-    .doto(() => { currentSheet += 1; })
+    .doto(() => {
+      currentSheet += 1;
+    })
     .reduce((wb, sheet) => {
-      const ws = XLSX.utils.json_to_sheet(sheet, { header: Object.keys(headers), skipHeader: true });
+      const ws = XLSX.utils.json_to_sheet(sheet, {
+        header: Object.keys(headers),
+        skipHeader: true
+      });
       XLSX.utils.book_append_sheet(wb, ws, `sheet${currentSheet}`);
       return wb;
     }, XLSX.utils.book_new())
-    .map(wb => XLSX.writeFile(wb, `${reportName}-book${currentBook}-${new Date()}.xlsx`));
+    .map(wb =>
+      XLSX.writeFile(wb, `${reportName}-book${currentBook}-${new Date()}.xlsx`)
+    );
 };
 
 module.exports = {
@@ -47,5 +100,5 @@ module.exports = {
   streamData,
   createBook,
   assignDocumentData,
+  createBookCustom
 };
-
